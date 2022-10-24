08f6142e2337d2c0026e0098893bb0003086b9a1
ElPinto110 <106269059+ElPinto110@users.noreply.github.com>
2022-10-23 23:07:21 -0400

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,14 @@
+const menu = document.querySelector('.tres')
+const menuContainer = document.querySelector('.menu-container')
+const close = document.querySelector('.xs')
+
+
+menu.addEventListener('click', ()=>{
+    menuContainer.classList.toggle('spread')
+    menu.classList.toggle('out')
+})
+
+close.addEventListener('click', ()=>{
+    menuContainer.classList.toggle('spread')
+    menu.classList.toggle('out')
+})
