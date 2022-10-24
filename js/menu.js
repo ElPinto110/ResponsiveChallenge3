2f7639db3ab73bd7cda7ef76d597f506f84d0682
ElPinto110 <106269059+ElPinto110@users.noreply.github.com>
2022-10-24 15:55:24 -0400

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -6,9 +6,11 @@ const close = document.querySelector('.xs')
 menu.addEventListener('click', ()=>{
     menuContainer.classList.toggle('spread')
     menu.classList.toggle('out')
+    close.classList.toggle('aparece')
 })
 
 close.addEventListener('click', ()=>{
     menuContainer.classList.toggle('spread')
     menu.classList.toggle('out')
+    close.classList.toggle('aparece')
 })
